e4aef73aa30a6a14de6887d9435bff0215ce9d0c
sabidwebdeveloper798 <sabidahmed798@gmail.com>
2026-09-13 00:24:45 +0600

    sec

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,5 +1,6 @@
-import React from "react";
 import Reacts from "../assets/react.png";
+import stars from "../assets/star.png";
+import Vue from "../assets/Vuejs.png";
 
 const Technologies = () => {
   return (
@@ -10,7 +11,6 @@ const Technologies = () => {
           {/* React */}
           <div className="rounded-xl border border-gray-200 bg-white p-4 shadow-sm">
             <div className="mb-3 flex items-center justify-between">
-              {/* <div className="text-2xl">⚛️</div> */}
               <img src={Reacts} alt="" />
               <span className="rounded-full bg-blue-50 px-2 py-1 text-[10px] text-blue-500">
                 Popular
@@ -27,37 +27,44 @@ const Technologies = () => {
               interfaces.
             </p>
 
-            <div className="mt-4 flex justify-between text-[10px] text-gray-500">
+            <div className="grid grid-cols-4 mt-4  justify-between text-[10px] text-gray-500">
               <span>Frontend</span>
-              <span>Beginner Friendly</span>
-              <span className="text-yellow-500">★ 4.9</span>
+              <span className="justify-self-center ml-19">Beginner</span>
+              <img src={stars} alt="" className=" ml-28 h-[14px] w-[9px] " />
+              {/* <span className="">4.9</span> */}
+              <h2 className="text-bold ml-auto ">4.9</h2>
             </div>
 
             <button className="mt-3 w-full rounded-md bg-slate-950 py-2 text-[11px] font-medium text-white hover:bg-slate-800">
               Add to Stack
             </button>
           </div>
-
+          {/* -------------------------- */}
           {/* Vue.js */}
           <div className="rounded-xl border border-gray-200 bg-white p-4 shadow-sm">
             <div className="mb-3 flex items-center justify-between">
-              <div className="text-2xl">🟢</div>
-              <span className="rounded-full bg-green-50 px-2 py-1 text-[10px] text-green-500">
+              <img src={Vue} alt="" />
+              <span className="rounded-full bg-blue-50 px-2 py-1 text-[10px] text-blue-500">
                 Popular
               </span>
             </div>
 
-            <h2 className="text-base font-bold text-gray-900">Vue.js</h2>
+            <h2 className="text-base font-bold text-gray-900">React</h2>
 
             <p className="mt-2 h-12 text-[11px] leading-4 text-gray-500">
-              An approachable, performant and versatile framework for building
-              web interfaces.
+              A declarative, component-based
+              <br />
+              JavaScript library for building modern user
+              <br />
+              interfaces.
             </p>
 
-            <div className="mt-4 flex justify-between text-[10px] text-gray-500">
+            <div className="grid grid-cols-4 mt-4  justify-between text-[10px] text-gray-500">
               <span>Frontend</span>
-              <span>Beginner</span>
-              <span className="text-yellow-500">★ 4.8</span>
+              <span className="justify-self-center ml-19">Beginner</span>
+              <img src={stars} alt="" className=" ml-28 h-[14px] w-[9px] " />
+              {/* <span className="">4.9</span> */}
+              <h2 className="text-bold ml-auto ">4.9</h2>
             </div>
 
             <button className="mt-3 w-full rounded-md bg-slate-950 py-2 text-[11px] font-medium text-white hover:bg-slate-800">
